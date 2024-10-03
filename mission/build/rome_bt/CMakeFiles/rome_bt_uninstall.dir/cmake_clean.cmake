61d3e1c5376a676bd0f62fd0da8c1505861b40f9
luricl <luri.cl95@gmail.com>
2024-10-03 15:11:39 -0300

    reorganize

--- mission/build/rome_bt/CMakeFiles/rome_bt_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rome_bt_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rome_bt_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
