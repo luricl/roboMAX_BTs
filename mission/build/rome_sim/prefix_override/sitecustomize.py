d2c1531e0b2e05164bffbc2e50d3cf05fa92c026
luricl <luri.cl95@gmail.com>
2025-05-16 13:49:58 -0300

    update readme

--- mission/build/rome_sim/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/luricl/Documents/UnB/PIBIC/code/lab_samples_logistics/mission/install/rome_sim'
